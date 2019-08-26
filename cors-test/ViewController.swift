acfc9d27f9a8053633beb122140e67d67ccf33a2
Alastair Coote <alastair.coote@gmail.com>
2019-08-26 17:25:27 -0400

    Initial Commit

---
+++ cors-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cors-test
+//
+//  Created by Alastair on 8/26/19.
+//  Copyright © 2019 alastair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
